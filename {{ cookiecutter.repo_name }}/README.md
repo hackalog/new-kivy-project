20a51e86d5daf7cbbb0f87357d29fa4b937448e8
Kjell Wooding <kjell@wooding.org>
2019-12-09 20:59:59 -0800

    make more generic

---
+++ {{ cookiecutter.repo_name }}/README.md
@@ -0,0 +1,5 @@
+# {{ cookiecutter.project_name }}
+
+Author: {{ cookiecutter.author_name }} <{{ cookiecutter.author_email }}>
+
+See `LICENSE` for license information
---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/versions.py
@@ -0,0 +1,5 @@
+GIT_VERSION = "heads/makefile_clean-0-gbe77cca"
+
+BUILD = "2019120501"
+RELEASE_VERSION = "{{ cookiecutter.version }}"
+__version__ = RELEASE_VERSION
